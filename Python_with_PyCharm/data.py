028f2b11bc89db1a2bea3806ee52742527e2251c
sadovnikovvlad <sadovnikovvlad@ocloud.com>
2023-04-05 19:42:20 +0800

    3rd - auto formated PEP8

--- Python_with_PyCharm/data.py
+++ Python_with_PyCharm/data.py
@@ -1,16 +1,17 @@
 import numpy.random
 import pandas as pd
+
 import matplotlib.pyplot as plt
 
 
 def my_data():
-
     data = {"Roll-num": [10, 20, 30, 40, 50, 60, 70],
             "Age": [12, 14, 13, 12, 14, 13, 15],
             "NAME": ['John', 'Camili', 'Rheana', 'Joseph',
                      'Amanti', 'Alexa', 'Siri']}
 
-    df = pd.DataFrame(data)
-    df.Age.hist()
-    plt.show()
-    print(df)
+
+df = pd.DataFrame(data)
+df.Age.hist()
+plt.show()
+print(df)
