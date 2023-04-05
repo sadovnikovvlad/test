d30d4ed12fc747d155e43954fa629343e97e4e95
sadovnikovvlad <sadovnikovvlad@icloud.com>
2023-04-05 17:43:44 +0800

    2nd commit

--- Python_with_PyCharm/main.py
+++ Python_with_PyCharm/main.py
@@ -1,4 +1,7 @@
 # This is a sample Python script.
+import service
+from data import my_data
+
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -7,7 +10,8 @@
 def main(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+    print(service.printservice())
+    my_data()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
---
+++ Python_with_PyCharm/service.py
@@ -0,0 +1,2 @@
+def printservice():
+    print('printservice')
